fa0d2cd49955b2c71415aa0b449ef8770ba25288
Максим Швецов <mshvetsov@at-consulting.ru>
2017-03-29 14:02:21 +0300

    Поменял настройки webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ let path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractLess = new ExtractTextPlugin({
   filename: "[name].[contenthash].css",
-  disable: true
+  disable: false
 });
 
 const NODE_ENV = process.env.NODE_ENV || 'production';
@@ -16,7 +16,7 @@ module.exports = {
     './app/app'
   ],
 
-  devtool: ((NODE_ENV == 'development') ? '#inline-source-map' : false),
+  devtool: '#inline-source-map',
 
   devServer: {
     historyApiFallback: true
