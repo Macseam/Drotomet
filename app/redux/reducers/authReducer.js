3d02694aa65a06b37819773f92c4dd732bbf0912
Максим Швецов <mshvetsov@at-consulting.ru>
2017-04-11 18:07:03 +0300

    Немного убрал кода из экшенов

--- app/redux/reducers/authReducer.js
+++ app/redux/reducers/authReducer.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import * as AuthTypes from '../types/authTypes';
-
 const initialState = {
   authToken: null,
   chaptersList: null,
@@ -13,25 +11,25 @@ const initialState = {
 export default function authInfo(state = initialState, action) {
   switch (action.type) {
 
-    case AuthTypes.GET_HEADER_AUTH_TOKEN_REQUEST:
+    case 'GET_HEADER_AUTH_TOKEN_REQUEST':
       return { ...state, authToken: initialState.authToken, loading: true, loaded: false };
-    case AuthTypes.GET_HEADER_AUTH_TOKEN_SUCCESS:
+    case 'GET_HEADER_AUTH_TOKEN_SUCCESS':
       return { ...state, authToken: action.data, loading: false, loaded: true };
-    case AuthTypes.GET_HEADER_AUTH_TOKEN_FAILURE:
+    case 'GET_HEADER_AUTH_TOKEN_FAILURE':
       return { ...state, authToken: initialState.authToken, loading: false, loaded: true };
 
-    case AuthTypes.GET_CHAPTERS_LIST_REQUEST:
+    case 'GET_CHAPTERS_LIST_REQUEST':
       return { ...state, chaptersList: initialState.chaptersList, loading: true, loaded: false };
-    case AuthTypes.GET_CHAPTERS_LIST_SUCCESS:
+    case 'GET_CHAPTERS_LIST_SUCCESS':
       return { ...state, chaptersList: action.data, loading: false, loaded: true };
-    case AuthTypes.GET_CHAPTERS_LIST_FAILURE:
+    case 'GET_CHAPTERS_LIST_FAILURE':
       return { ...state, chaptersList: initialState.chaptersList, loading: false, loaded: true };
 
-    case AuthTypes.GET_ITEMS_LIST_REQUEST:
+    case 'GET_ITEMS_LIST_REQUEST':
       return { ...state, chapterItemsList: initialState.chapterItemsList, loading: true, loaded: false };
-    case AuthTypes.GET_ITEMS_LIST_SUCCESS:
+    case 'GET_ITEMS_LIST_SUCCESS':
       return { ...state, chapterItemsList: action.data, loading: false, loaded: true };
-    case AuthTypes.GET_ITEMS_LIST_FAILURE:
+    case 'GET_ITEMS_LIST_FAILURE':
       return { ...state, chapterItemsList: initialState.chapterItemsList, loading: false, loaded: true };
 
     default:
