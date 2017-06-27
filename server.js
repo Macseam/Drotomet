b8ad05c1ad24ac8d17a4dde2216d0facd6eb3887
Максим Швецов <mshvetsov@at-consulting.ru>
2017-06-27 15:09:59 +0300

    Снова пытаюсь угодить хероку

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-let express = require('express');
-let app = express();
+let webpack = require('webpack');
+let WebpackDevServer = require('webpack-dev-server');
+let config = require('./webpack.config');
 
-app.use(express.static(__dirname + '/'));
+if (process.env && process.env.PORT) {
+  new WebpackDevServer(webpack(config),{publicPath: '/build/'}).listen(8080, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
 
-app.listen(process.env.PORT || 8080);
+    console.log('Listening at localhost:8080');
+  });
+}
+else {
+  let express = require('express');
+  let app = express();
+
+  app.use(express.static(__dirname + '/'));
+
+  app.listen(process.env.PORT || 8080);
+}
