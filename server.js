74bac37e7a4c4b29d36aaa5e42c628a2e8d5535a
Максим Швецов <mshvetsov@at-consulting.ru>
2017-06-27 15:29:21 +0300

    Перетащил вебпак в dependencies

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ let WebpackDevServer = require('webpack-dev-server');
 let config = require('./webpack.config');
 
 if (process.env && process.env.PORT) {
+  let express = require('express');
+  let app = express();
+
+  app.use(express.static(__dirname + '/'));
+
+  app.listen(process.env.PORT || 8080);
+}
+else {
   new WebpackDevServer(webpack(config),{publicPath: '/build/'}).listen(8080, 'localhost', function (err, result) {
     if (err) {
       console.log(err);
@@ -10,12 +18,4 @@ if (process.env && process.env.PORT) {
 
     console.log('Listening at localhost:8080');
   });
-}
-else {
-  let express = require('express');
-  let app = express();
-
-  app.use(express.static(__dirname + '/'));
-
-  app.listen(process.env.PORT || 8080);
 }
