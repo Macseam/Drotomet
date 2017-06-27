21122c2a4e8e6a249126aac55910c3107b44125e
Максим Швецов <mshvetsov@at-consulting.ru>
2017-06-27 15:04:03 +0300

    Исправил файл на хероку

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-let webpack = require('webpack');
-let WebpackDevServer = require('webpack-dev-server');
-let config = require('./webpack.config');
+let express = require('express');
+let app = express();
 
-new WebpackDevServer(webpack(config),{publicPath: '/build/'}).listen(process.env.PORT || 8080, 'localhost', function (err, result) {
-        if (err) {
-            console.log(err);
-        }
+app.use(express.static(__dirname + '/'));
 
-        console.log('Listening at localhost:8080');
-    });
+app.listen(process.env.PORT || 8080);
